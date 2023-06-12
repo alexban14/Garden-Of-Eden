54afa5aff20f44ca588e9c7a09647f8a57b927a0
Elena <vioricaelena.moldovanu@ulbsibiu.ro>
2023-06-12 20:03:21 +0300

    fixed the greeting name to be dynamic for the specialits booking page

--- garden_client/src/environments/environment.development.ts
+++ garden_client/src/environments/environment.development.ts
@@ -3,7 +3,7 @@ export const environment = {
 	serverURL: 'http://127.0.0.1:8000',
 	tinyApiKey: 'jluieng8zgbd8u7irzgxbvm3s0mphvz8d167awux818fqz9u',
 	// client secret token ID for Oauth authentication
-	CLIENT_SECRET: '4c1NeBCzZJAimTDLLp7flKbU97wrTSs6jWqRYLJH',
+	CLIENT_SECRET: '3QW3492OvLLZNRLFfPEBbwMpPGtpfg6VBpVFmtxN',
 	// client ID for Oauth authentication
 	CLIENT_ID: 2
 };
